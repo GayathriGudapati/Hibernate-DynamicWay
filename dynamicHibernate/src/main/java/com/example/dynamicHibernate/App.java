006605aec544620e0e1e0e764de1964d8bf53e07
BALA GAYATHRI GUDAPATI <152675023+GayathriGudapati@users.noreply.github.com>
2024-08-02 22:29:36 +0530

    Hibernate (dynamic way)

---
+++ dynamicHibernate/src/main/java/com/example/dynamicHibernate/App.java
@@ -0,0 +1,13 @@
+package com.example.dynamicHibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
